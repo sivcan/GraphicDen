276e96bccef196479b000070f32255d1ebc42e12
Sivcan Singh <sivcansingh@me.com>
2016-03-26 18:01:21 +0530

    Base Layout Ready

--- graphs/views.py
+++ graphs/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
 import datetime
-
+from django.utils.safestring import mark_safe
 from blog.forms import GraphForm
 from blog.models import User
 
@@ -10,6 +10,7 @@ import json
 
 import os
 
+json_data=[]
 
 def adder(request):
 	if request.method=='POST':
@@ -40,17 +41,62 @@ def converter(request):
 	csvfile = open(p, 'r')
 	jsonfilewr+='djangojson.json'
 	jsonfile = open(jsonfilewr, 'w')
-
-	fieldnames = ("date","value")
+	#json_data=[]
+	i=1
+	fieldnames = ("date","data3")
 	reader = csv.DictReader(csvfile,fieldnames)
 	for row in reader:
 		json.dump(row, jsonfile)
-		jsonfile.write('\n')
-		
-	return redirect ('/hardcode/')
+		#jsonfile.write(',\n')
+		#print ("" + str(row))
+		if i != 1:
+			json_data.append(row)
+		i=i+1
+
+	print(str(json_data))
+
+	return redirect ('/success/')
 	#return render(request, 'graph.html', {"p":jsonfilewr})
 
 
 def hardcode(request):
-	p="[{ key: 0, value: 5 },{ key: 1, value: 10 },{ key: 2, value: 13 },{ key: 3, value: 19 },{ key: 4, value: 21 },{ key: 5, value: 25 },{ key: 6, value: 22 },{ key: 7, value: 18 },{ key: 8, value: 15 },{ key: 9, value: 13 },{ key: 10, value: 11 },{ key: 11, value: 12 },{ key: 12, value: 15 },{ key: 13, value: 20 },{ key: 14, value: 18 },{ key: 15, value: 17 },{ key: 16, value: 16 },{ key: 17, value: 18 },{ key: 18, value: 23 },{ key: 19, value: 29 } ];"
-	return render(request, 'sampled3.html', locals())
+	#p="[{ key: 0, data3: 5 },{ key: 1, data3: 10 },{ key: 2, data3: 13 },{ key: 3, data3: 19 },{ key: 4, data3: 21 },{ key: 5, data3: 25 },{ key: 6, data3: 22 },{ key: 7, data3: 18 },{ key: 8, data3: 15 },{ key: 9, data3: 13 },{ key: 10, data3: 11 },{ key: 11, data3: 12 },{ key: 12, data3: 15 },{ key: 13, data3: 20 },{ key: 14, data3: 18 },{ key: 15, data3: 17 },{ key: 16, data3: 16 },{ key: 17, data3: 18 },{ key: 18, data3: 23 },{ key: 19, data3: 29 } ];"
+	# p=[{"velocity": "202","time": "2000"},
+	# 	{"velocity": "215","time": "2002"},
+	# 	{"velocity": "179","time": "2004"},
+	# 	{"velocity": "199","time": "2006"}]
+	# p=[{
+	# 	"velocity": "202",
+	# 	"time": "2000"
+	# }, {
+	# 	"velocity": "215",
+	# 	"time": "2002"
+	# }, {
+	# 	"velocity": "179",
+	# 	"time": "2004"
+	# }, {
+	# 	"velocity": "199",
+	# 	"time": "2006"
+	# }, {
+	# 	"velocity": "134",
+	# 	"time": "2008"
+	# }, {
+	# 	"velocity": "176",
+	# 	"time": "2010"
+	# }]
+	# p=[{"date": "41", "data3": "153"},
+	# 	{"date": "42", "data3": "165"},
+	# 	{"date": "43", "data3": "269"},
+	# 	{"date": "44", "data3": "344"},
+	# 	{"date": "45", "data3": "376"},
+	# 	{"date": "56", "data3": "410"},
+	# 	{"date": "57", "data3": "421"},
+	# 	{"date": "68", "data3": "405"},
+	# 	{"date": "79", "data3": "376"},
+	# 	{"date": "80", "data3": "359"},
+	# 	{"date": "91", "data3": "392"},
+	# 	{"date": "112", "data3": "433"},
+	# 	{"date": "113", "data3": "455"},
+	# 	{"date": "114", "data3": "478"}]
+	print(str(json_data))
+	return render(request, "khatterd3.html", { "p" : json.dumps(json_data)})
